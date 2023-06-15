91984188cf971da349801b7c00b80b9b6bf9e39a
LASmelov <115341489+LASmelov@users.noreply.github.com>
2023-06-15 18:24:48 +0300

    Add files via upload

---
+++ hello/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from firstapp import views
+
+urlpatterns = [
+path('', views.index, name='home'),
+path('about/', views.about),
+path('contact/', views.contact),
+path('details/', views.details),
+]
